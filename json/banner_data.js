fc6f1cea3f588c6c182a864d570d3200f2fbf95f
coderccb <2650191601@qq.com>
2023-06-24 17:51:21 +0800

    完成华为商城轮播图transform

---
+++ json/banner_data.js
@@ -0,0 +1,148 @@
+var banners = [
+  {
+    "actionUrlWap": "",
+    "actionUrlWeb": "https://www.vmall.com/portal/activity/index.html?pn=zhuhuichangpc",
+    "actonUrl": "",
+    "bgColor": "#ED2E35",
+    "dataSourceCode": "DB000044",
+    "dataSourceType": "ad",
+    "id": 20533,
+    "imgUrl": "/cmscdn/CN/2023-06/917d846962da438097c5e3b317124207.jpg",
+    "imgWebpUrlMap": {
+      "webp_1": "/uomcdn/CN/cms/202202/F1B873739558FB5D1059099F4220ABA4.jpg.25.webp",
+      "webp_2": "/uomcdn/CN/cms/202202/F1B873739558FB5D1059099F4220ABA4.jpg.50.webp",
+      "webp_3": "/uomcdn/CN/cms/202202/F1B873739558FB5D1059099F4220ABA4.jpg.75.webp",
+      "webp_4": "/uomcdn/CN/cms/202202/F1B873739558FB5D1059099F4220ABA4.jpg.webp"
+    },
+    "tagSelected": "{}",
+    "title": "主"
+  },
+  {
+    "actionUrlWap": "",
+    "actionUrlWeb": "https://www.vmall.com/portal/activity/index.html?pn=huaweizonePC",
+    "actonUrl": "",
+    "bgColor": "#ED2E35",
+    "dataSourceCode": "DB000044",
+    "dataSourceType": "ad",
+    "id": 17691,
+    "imgUrl": "/cmscdn/CN/2023-06/215c3d1ece06474d8bb7b33ff111decf.jpg",
+    "imgWebpUrlMap": {
+      "webp_1": "/uomcdn/CN/cms/202202/9B8652362711564FEB2B33793250D8B9.png.25.webp",
+      "webp_2": "/uomcdn/CN/cms/202202/9B8652362711564FEB2B33793250D8B9.png.50.webp",
+      "webp_3": "/uomcdn/CN/cms/202202/9B8652362711564FEB2B33793250D8B9.png.75.webp",
+      "webp_4": "/uomcdn/CN/cms/202202/9B8652362711564FEB2B33793250D8B9.png.webp"
+    },
+    "tagSelected": "{}",
+    "title": "华为"
+  },
+  {
+    "actionUrlWap": "",
+    "actionUrlWeb": "https://www.vmall.com/portal/activity/index.html?pageId=401022361",
+    "actonUrl": "",
+    "bgColor": "#ED2E35",
+    "dataSourceCode": "DB000044",
+    "dataSourceType": "ad",
+    "id": 839,
+    "imgUrl": "/cmscdn/CN/2023-06/c2f9508833e9480ea4be91cd22dac43e.jpg",
+    "imgWebpUrlMap": {
+      "webp_1": "/uomcdn/CN/cms/202202/55338EF54406E2D3D6FAC2BF6C9D231D.png.25.webp",
+      "webp_2": "/uomcdn/CN/cms/202202/55338EF54406E2D3D6FAC2BF6C9D231D.png.50.webp",
+      "webp_3": "/uomcdn/CN/cms/202202/55338EF54406E2D3D6FAC2BF6C9D231D.png.75.webp",
+      "webp_4": "/uomcdn/CN/cms/202202/55338EF54406E2D3D6FAC2BF6C9D231D.png.webp"
+    },
+    "tagSelected": "{}",
+    "title": "教育办公"
+  },
+  {
+    "actionUrlMp": "",
+    "actionUrlWap": "",
+    "actionUrlWeb": "https://www.vmall.com/portal/activity/index.html?pn=harmonyosconnectpc",
+    "actonUrl": "",
+    "bgColor": "#ED2E35",
+    "dataSourceCode": "DB000044",
+    "dataSourceType": "ad",
+    "id": 29135,
+    "imgUrl": "/cmscdn/CN/2023-06/8f4794ebf4994c0088e4c3734af5dc27.jpg",
+    "imgWebpUrlMap": {
+      "webp_1": "/uomcdn/CN/cms/202202/B53ABB8B90DC11AABDDE6F379FF57866.png.25.webp",
+      "webp_2": "/uomcdn/CN/cms/202202/B53ABB8B90DC11AABDDE6F379FF57866.png.50.webp",
+      "webp_3": "/uomcdn/CN/cms/202202/B53ABB8B90DC11AABDDE6F379FF57866.png.75.webp",
+      "webp_4": "/uomcdn/CN/cms/202202/B53ABB8B90DC11AABDDE6F379FF57866.png.webp"
+    },
+    "tagSelected": "{}",
+    "title": "行业"
+  },
+  {
+    "actionUrlWap": "",
+    "actionUrlWeb": "https://www.vmall.com/portal/activity/index.html?pn=zhixuanpc",
+    "actonUrl": "",
+    "bgColor": "#ED2E35",
+    "dataSourceCode": "DB000044",
+    "dataSourceType": "ad",
+    "id": 9059,
+    "imgUrl": "/cmscdn/CN/2023-06/914d1984b60d460bb9830318a33daaea.jpg",
+    "imgWebpUrlMap": {
+      "webp_1": "/uomcdn/CN/cms/202202/15550D184131C453A2CED9E443AEDDF8.jpg.25.webp",
+      "webp_2": "/uomcdn/CN/cms/202202/15550D184131C453A2CED9E443AEDDF8.jpg.50.webp",
+      "webp_3": "/uomcdn/CN/cms/202202/15550D184131C453A2CED9E443AEDDF8.jpg.75.webp",
+      "webp_4": "/uomcdn/CN/cms/202202/15550D184131C453A2CED9E443AEDDF8.jpg.webp"
+    },
+    "tagSelected": "{}",
+    "title": "智选"
+  },
+  {
+    "actionUrlWap": "",
+    "actionUrlWeb": "https://www.vmall.com/portal/activity/index.html?pn=zhcxaitoweb",
+    "actonUrl": "",
+    "bgColor": "#ED2E35",
+    "dataSourceCode": "DB000044",
+    "dataSourceType": "ad",
+    "id": 18893,
+    "imgUrl": "/cmscdn/CN/2023-06/c03d07c8b9534edf960117850562f489.jpg",
+    "imgWebpUrlMap": {
+      "webp_1": "/uomcdn/CN/cms/202201/3E994BF010D07F8CD8E8B445514A06F5.png.25.webp",
+      "webp_2": "/uomcdn/CN/cms/202201/3E994BF010D07F8CD8E8B445514A06F5.png.50.webp",
+      "webp_3": "/uomcdn/CN/cms/202201/3E994BF010D07F8CD8E8B445514A06F5.png.75.webp",
+      "webp_4": "/uomcdn/CN/cms/202201/3E994BF010D07F8CD8E8B445514A06F5.png.webp"
+    },
+    "tagSelected": "{}",
+    "title": "AITO"
+  },
+  {
+    "actionUrlMp": "",
+    "actionUrlWap": "",
+    "actionUrlWeb": "https://www.vmall.com/portal/activity/index.html?pageId=401021384",
+    "actonUrl": "",
+    "bgColor": "#ED2E35",
+    "dataSourceCode": "DB000044",
+    "dataSourceType": "ad",
+    "id": 29439,
+    "imgUrl": "/cmscdn/CN/2023-06/4c13c2f45a1c4674a37094a4fd249527.png",
+    "imgWebpUrlMap": {
+      "webp_1": "/uomcdn/CN/cms/202202/B1726D36C05F0486A3D07605F34A4C7D.png.25.webp",
+      "webp_2": "/uomcdn/CN/cms/202202/B1726D36C05F0486A3D07605F34A4C7D.png.50.webp",
+      "webp_3": "/uomcdn/CN/cms/202202/B1726D36C05F0486A3D07605F34A4C7D.png.75.webp",
+      "webp_4": "/uomcdn/CN/cms/202202/B1726D36C05F0486A3D07605F34A4C7D.png.webp"
+    },
+    "tagSelected": "{}",
+    "title": "智慧办公"
+  },
+  {
+    "actionUrlWap": "",
+    "actionUrlWeb": "https://www.vmall.com/portal/activity/index.html?pageId=301002069",
+    "actonUrl": "",
+    "bgColor": "#ED2E35",
+    "dataSourceCode": "DB000044",
+    "dataSourceType": "ad",
+    "id": 19369,
+    "imgUrl": "/cmscdn/CN/2023-06/f410cd86ed6c45d8a087ffa80628fe55.jpg",
+    "imgWebpUrlMap": {
+      "webp_1": "/uomcdn/CN/cms/202202/083AA2A10A88835A2BE7A786DC18F85F.jpg.25.webp",
+      "webp_2": "/uomcdn/CN/cms/202202/083AA2A10A88835A2BE7A786DC18F85F.jpg.50.webp",
+      "webp_3": "/uomcdn/CN/cms/202202/083AA2A10A88835A2BE7A786DC18F85F.jpg.75.webp",
+      "webp_4": "/uomcdn/CN/cms/202202/083AA2A10A88835A2BE7A786DC18F85F.jpg.webp"
+    },
+    "tagSelected": "{}",
+    "title": "莫塞尔"
+  }
+]
